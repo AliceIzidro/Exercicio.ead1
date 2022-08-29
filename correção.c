2f97d5406f2fb7d1c3b574f3be1ccb77da96e8c5
Alice Izidro <111870253+AliceIzidro@users.noreply.github.com>
2022-08-29 10:15:57 -0300

    Add files via upload

---
+++ correção.c
@@ -0,0 +1,52 @@
+#include<locale.h>
+#include<stdio.h>
+ int main(){
+	setlocale(LC_ALL,"");
+	int p1= -1, p2 = -1, p3 = -1, p4 = -1, p5 = -1;
+	//entradas
+	while(p1 <0|| p1 > 1){
+		printf("Telefonou para a vítima? 0.não, 1.sim");
+		scanf("%d", &p1);
+	}   
+		while(p2 <0|| p2 > 1){
+		printf("Esteve no local do crime? 0.não, 1.sim");
+		scanf("%d", &p2);
+	} 
+		while(p3 <0|| p3 > 1){
+		printf("Mora perto da vítima? 0.não, 1.sim");
+		scanf("%d", &p3);
+	}
+		while(p4 <0|| p4 > 1){
+		printf("Devia para a vítima? 0.não, 1.sim");
+		scanf("%d", &p4);
+	} 
+		while(p5 <0|| p5 > 1){
+		printf("Já trabalhou com a vítima? 0.não, 1.sim");
+		scanf("%d", &p5);
+		}
+		//processamento
+		int total= p1 + p2 + p3 + p4 + p5;
+	if(total <2){
+	    printf("Esrá liberado.");
+    }else if(total <3){
+    	printf("Você é suspeito.");
+    }else if(total <5){
+	    printf("Você é cumplice.");
+    }else{
+	    printf("Você é assasino.");
+	}
+		}
+	
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
